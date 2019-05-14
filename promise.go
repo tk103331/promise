af4b235a15364848ba79d5a45a01747e590b6d09
tk103331 <tk103331@gmail.com>
2019-05-14 17:41:32 +0800

    refactor

--- promise.go
+++ promise.go
@@ -5,99 +5,116 @@ import (
 	"sync"
 )
 
-type handler func(interface{}) interface{}
-type executor func(handler, handler)
+type function func(interface{}) interface{}
+type consumer func(interface{})
+type executor func(consumer, consumer)
 
 type status int
 
-var sRESOLVED status = 1
-var sREJECTED status = 2
+const sPENDING status = 0
+const sRESOLVED status = 1
+const sREJECTED status = 2
 
 type Promise struct {
-	stat status
-	data interface{}
-	lock sync.Mutex
-	res  handler
-	rej  handler
-	next *Promise
-}
-
-func (p *Promise) resolve(v interface{}) interface{} {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-	p.data = v
-	p.stat = sRESOLVED
-
-	if p.res != nil {
-		fmt.Printf("%p resolve1\n", p)
-		d := p.res(p.data)
-		fmt.Printf("%p resolve11\n", p)
-		if p.next != nil {
-			p.next.resolve(d)
-		}
-	} else if p.next != nil {
-		fmt.Printf("%p resolve2\n", p)
-		p.next.resolve(p.data)
-		fmt.Printf("%p resolve21\n", p)
-	}
-	return nil
-}
-
-func (p *Promise) reject(v interface{}) interface{} {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-	p.data = v
-	p.stat = sREJECTED
-
-	if p.rej != nil {
-		d := p.rej(v)
-		if p.next != nil {
-			p.next.resolve(d)
-		}
-	} else if p.next != nil {
-		return p.next.reject(p.data)
-	}
-	return nil
-}
-
-func (p *Promise) handleResult(h handler) *Promise {
-	result := h(p.data)
-	fmt.Printf("%p ", p)
-	if pp, ok := result.(*Promise); ok {
-		fmt.Printf("%p handleResult1\n", p)
-		return pp
-	} else {
-		fmt.Printf("%p handleResult2\n", p)
-		return Resolve(result)
-	}
-}
-
-func (p *Promise) Then(onResolve handler, onReject handler) *Promise {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-	fmt.Printf("%p ", p)
-	if p.stat == sRESOLVED {
-		fmt.Println("then res ")
-		return p.handleResult(onResolve)
-	} else if p.stat == sREJECTED {
-		fmt.Println("then rej ")
-		return p.handleResult(onReject)
-	} else {
-		fmt.Println("then else ")
-		newP := &Promise{res: onResolve, rej: onReject}
-		p.next = newP
-		return newP
-	}
-}
-
-func Resolve(v interface{}) *Promise {
-	return New(func(res handler, rej handler) {
-		res(v)
-	})
+	stat       status
+	value      interface{}
+	err        error
+	lock       sync.Mutex
+	resHandler consumer
+	rejHandler consumer
+	errHandler consumer
+	next       *Promise
 }
 
 func New(exec executor) *Promise {
 	p := &Promise{}
-	exec(p.resolve, p.reject)
+	exec(func(v interface{}) {
+		p.handleRes(v)
+	}, func(v interface{}) {
+		p.handleRej(v)
+	})
 	return p
+}
+
+func (p *Promise) handleRes(v interface{}) {
+	if p.stat != sPENDING {
+		return
+	}
+	p.stat = sRESOLVED
+	p.value = v
+	if p.next != nil && p.next.resHandler != nil {
+		p.next.resHandler(v)
+	}
+}
+
+func (p *Promise) handleRej(v interface{}) {
+	if p.stat != sPENDING {
+		return
+	}
+	p.stat = sREJECTED
+	p.value = v
+	if p.next != nil && p.next.rejHandler != nil {
+		p.next.rejHandler(v)
+	}
+}
+
+func (p *Promise) handleCatch(err error) {
+	if p.stat != sPENDING {
+		return
+	}
+	p.stat = sREJECTED
+	p.value = err
+	nextP := p.next
+	for {
+		if nextP != nil {
+			errHandler := p.next.errHandler
+			if errHandler != nil {
+				errHandler(err)
+				break
+			}
+			nextP = nextP.next
+		}
+	}
+
+}
+
+func (p *Promise) Then(onRes function, onRej function) *Promise {
+	newP := &Promise{}
+	if p.stat == sPENDING {
+		p.next = newP
+		newP.resHandler = func(v interface{}) {
+			fmt.Println("resh")
+			if onRes != nil {
+				newP.handleRes(onRes(v))
+			}
+		}
+		newP.rejHandler = func(v interface{}) {
+			fmt.Println("rejh")
+			if onRej != nil {
+				newP.handleRes(onRej(v))
+			}
+		}
+	} else if p.stat == sRESOLVED {
+		fmt.Println("res")
+		if onRes != nil {
+			newP.handleRes(onRes(p.value))
+		}
+	} else if p.stat == sREJECTED {
+		fmt.Println("rej")
+		if onRej != nil {
+			newP.handleRes(onRej(p.value))
+		}
+	}
+	return newP
+}
+
+func (p *Promise) Catch(onErr function) *Promise {
+	newP := &Promise{}
+	p.next = newP
+	if p.stat == sPENDING {
+		newP.errHandler = func(err interface{}) {
+			onErr(err)
+		}
+	}
+	return newP
 }
--- promise_test.go
+++ promise_test.go
@@ -9,29 +9,33 @@ func asyncTask(task func()) {
 	go task()
 }
 
-func TestThenResolve(t *testing.T) {
+// func TestResolve(t *testing.T) {
 
-	New(func(resolve handler, reject handler) {
+// 	New(func(resolve consumer, reject consumer) {
+// 		asyncTask(func() {
+// 			resolve("resolved value")
+// 		})
+// 	}).Then(func(v interface{}) interface{} {
+// 		fmt.Println("TestResolve result1:", v)
+// 		return "another resolved value"
+// 	}, nil).Then(func(v interface{}) interface{} {
+// 		fmt.Println("TestThenResolve result2:", v)
+// 		return nil
+// 	}, nil)
+// 	fmt.Println("TestResolve")
+// }
+
+func TestReject(t *testing.T) {
+	New(func(resolve consumer, reject consumer) {
 		asyncTask(func() {
-			resolve("resolved value")
+			reject("rejected value")
 		})
+	}).Then(nil, func(v interface{}) interface{} {
+		fmt.Println("TestReject result1:", v)
+		return "resolve value"
 	}).Then(func(v interface{}) interface{} {
-		fmt.Println("TestThenResolve result1:", v)
-		return "another resolved value"
-	}, nil).Then(func(v interface{}) interface{} {
-		fmt.Println("TestThenResolve result2:", v)
+		fmt.Println("TestReject result2:", v)
 		return nil
 	}, nil)
-	fmt.Println("...")
-}
-
-func TestThenReject(t *testing.T) {
-	// New(func(resolve handler, reject handler) {
-	// 	asyncTask(func() {
-	// 		resolve("rejected value")
-	// 	})
-	// }).Then(nil, func(v interface{}) interface{} {
-	// 	fmt.Println("TestThenReject result:", v)
-	// 	return nil
-	// })
+	fmt.Println("TestReject")
 }
